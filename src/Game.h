2b34dde0e2f6fce6bcb855ddec8d0eee985d0f5e
sn1675 <nathcourbet@gmail.com>
2024-12-11 14:35:53 +0100

    tkt frr

--- src/Game.h
+++ src/Game.h
@@ -1,14 +1,27 @@
+#include <iostream>
 #include "Plateau.h"
 
 class Game {
     public:
         Game(){
-            Plateau P = Plateau();
             run();
         }
 
     private:
+        bool r = true;
+        Plateau P = Plateau();
+
+        std::string choix;
+
         void run(){
+
+            while (r) {
+                P.afficher();
+
+                std::cout << "Choisisez un mouvement [Dep][Arrive] :"; std::cin >> choix;
+
+                P.deplacerPiece(std::string() + choix[0]+choix[1], std::string() + choix[2]+choix[3]);
+            }
 
         }
 };
