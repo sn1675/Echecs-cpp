0775541a3ef67d76674028696a5aa0a3a9013858
sn1675 <nathcourbet@gmail.com>
2025-02-27 09:05:22 +0100

    creation de la classe timer

---
+++ src/Timer.h
@@ -0,0 +1,34 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <time.h>
+
+class Timer{
+    public:
+
+        Timer(float t, float increment = 0){
+            /* t représente le temps que chaque joueur possède a la pandule
+             Exemple :
+                10.0 = 10 minutes
+                3.0 = 3 minutes
+                0.3 = 30 secondes
+            increment est a 0 par défault
+             */
+        }
+
+        void startTimer(){
+
+        }
+
+        void stopTimer(){
+
+        }
+
+        void resertTimer(){
+
+        }
+
+    private:
+};
+
+#endif
